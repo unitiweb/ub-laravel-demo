a189dfb91ebe97348a7cf3a5e041fe8c570a3f53
= <=>
2021-01-10 21:03:33 -0700

    Some updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
         // removeDeprecatedGapUtilities: true,
         // purgeLayersByDefault: true,
     },
-    purge: [],
+    purge: false,
     darkMode: false,
     theme: {
         extend: {
